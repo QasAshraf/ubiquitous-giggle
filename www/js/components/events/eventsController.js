5def358dfce3942aff6d374ef605228e5f8e863c
Ben Marshall <ben3005@hotmail.com>
2015-10-24 14:32:54 +0100

    fixed dependenices for npm, added apiService

--- www/js/components/events/eventsController.js
+++ www/js/components/events/eventsController.js
@@ -3,7 +3,8 @@
 
     var controllerModule = angular.module('volunteasy.controllers');
 
-    controllerModule.controller('eventsController', function ($scope) {
+    controllerModule.controller('eventsController', function ($scope, apiService) {
         console.log('eventsController init');
+        apiService.testMethod();
     });
 })();
---
+++ www/js/shared/services/apiService.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict'
+
+    var serviceModule = angular.module('volunteasy.services');
+
+    serviceModule.factory('apiService', function ($http, $q) {
+        var service = {};
+
+        service.testMethod = function () {
+            console.log('apiService - testMethod');
+        };
+
+
+        return service;
+    });
+
+})();
