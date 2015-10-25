0bf95e61cb922450c85419f2373c59b15da80c07
Ben Marshall <ben3005@hotmail.com>
2015-10-25 01:09:24 +0000

    implemented social aspects

--- www/js/components/event/eventController.js
+++ www/js/components/event/eventController.js
@@ -3,10 +3,55 @@
 
     var controllerModule = angular.module('volunteasy.controllers');
 
-    controllerModule.controller('eventController', function ($scope, $stateParams, apiService) {
+    controllerModule.controller('eventController', function ($scope, $stateParams, $cordovaSocialSharing, apiService) {
         $scope.event = {};
 
         $scope.zoom = 12;
+
+        $scope.twitterShare = function () {
+            var message = 'Im going to ' + $scope.event.name;
+
+            if (message.length >= 129)
+                message = message.substring(0, 126) + '...';
+
+            message = message + ' #Voluntezy';
+
+            $cordovaSocialSharing
+                .shareViaTwitter(message)
+                .then(
+                    function (success) {
+                        console.log('eventController - twitter share completed');
+                        console.log(success);
+                    },
+                    function (error) {
+                        console.log('eventController - twitter share failed');
+                        console.log(error);
+                    }
+                );
+        };
+
+        $scope.share = function () {
+            var message = 'Im going to ' + $scope.event.name;
+
+            if (message.length >= 129)
+                message = message.substring(0, 126) + '...';
+
+            message = message + ' #Voluntezy';
+
+            $cordovaSocialSharing
+                .share(message)
+                .then(
+                    function (success) {
+                        console.log('eventController - share completed');
+                        console.log(success);
+                    },
+                    function (error) {
+                        console.log('eventController - share failed');
+                        console.log(error);
+                    }
+                );
+        };
+
 
         apiService.getEvent($stateParams.eventId)
         .then(function (data) {
